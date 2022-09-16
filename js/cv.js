3fa65ffd1a69f2b23f7c994262651e6108058537
mahmoud elboushy <101577719+mahmoudb192000@users.noreply.github.com>
2022-09-17 00:16:54 +0200

    u

--- js/cv.js
+++ js/cv.js
@@ -128,3 +128,8 @@ function cft8(){
     show.style.display="block";
     show.innerHTML='<p style="font-size: 50px; position: absolute; top: 2%; left:48%; cursor: pointer;margin:0px" onclick="hcft1()">×</p><br><center><div style="width: 80%; height: 500px; position: absolute; top: 10%; left:10%;overflow: auto;"><img src="img/11.jpg" style="width: 100%;height:auto;"></div></center>';
     }
+    function cft9(){
+        var show=document.getElementById("dscft");
+        show.style.display="block";
+        show.innerHTML='<p style="font-size: 50px; position: absolute; top: 2%; left:48%; cursor: pointer;margin:0px" onclick="hcft1()">×</p><br><img src="img/9.jpg" style="width: 80%; height: 500px; position: fixed; top: 10%; left:10%">';
+        }
